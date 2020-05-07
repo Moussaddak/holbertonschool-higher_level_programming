23117be1661075d55528af23bd3dc7d4d89b98ff
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-07 20:56:05 +0000

    tasks

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
-    if idx > len(my_list):
+    if idx > len(my_list) or idx < 0:
         return (my_list)
     for i in my_list:
         if my_list.index(i) is idx:
