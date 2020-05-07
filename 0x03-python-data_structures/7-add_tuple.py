48d6e4f4796d419e970eea03e19ab7274c0b2497
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-07 17:37:36 +0000

    tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) < 2:
+        l = list(tuple_a)
+        for i in range(2 - len(tuple_a)):
+            l.append(0)
+        tuple_a = tuple(l)
+    if len(tuple_b) < 2:
+        l = list(tuple_b)
+        for i in range(2 - len(tuple_b)):
+            l.append(0)
+        tuple_b = tuple(l)
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
