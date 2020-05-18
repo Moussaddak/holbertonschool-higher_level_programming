15e3b9fea068198c82729c9fbfe6cb80706d1963
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-18 22:54:38 +0000

    tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {}".format(error), file=stderr)
+        return False
