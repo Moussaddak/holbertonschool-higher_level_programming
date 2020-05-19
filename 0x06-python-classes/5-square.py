9fdbfd3be7b93ab7b5c93a33aad45cfc74b5628a
moussaddak <meddeb.moussaddak@gmail.com>
2020-05-19 22:41:21 +0000

    tasks

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""documentation of method"""
+
+
+class Square:
+    """define square class"""
+    def __init__(self, size=0):
+        """Initializes the square size."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the value of size (private object variable)"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ set the size value of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculate Area of the square"""
+        return(self.__size ** 2)
+
+    def my_print(self):
+        """draw a square of # """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
+
+        if not self.__size:
+            print()
