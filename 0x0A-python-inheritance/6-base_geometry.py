3dcbe9fe6305147279736a41a0681c5de73fa0dd
moussaddak <meddeb.moussaddak@gmail.com>
2020-06-02 22:17:24 +0000

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Documentation of class BaseGeometry """
+
+
+class BaseGeometry:
+    """ empty class BaseGeometry """
+    def area(self):
+        raise "area() is not implemented"
