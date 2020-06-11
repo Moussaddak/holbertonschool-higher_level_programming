2572a1fc44a0dd920a8bad445993df757a131a03
moussaddak <meddeb.moussaddak@gmail.com>
2020-06-11 22:36:33 +0000

    tasks

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""documentation of method"""
+
+
+class Square:
+    """define square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square size."""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Retrieve the value of size (private object variable)"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set the size value of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculate Area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """draw a square of # """
+        if not self.__size:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end='')
+            for j in range(self.__size):
+                print("#", end='')
+            print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ set position value of Square"""
+        try:
+            if not type(value) is tuple:
+                raise Exception
+            elif not (len(value) == 2 and type(value[0]) is int and
+                      type(value[1]) is int):
+                raise Exception
+            elif not (value[0] > 0 and value[1] > 0):
+                raise Exception
+        except TypeError:
+            print("position must be a tuple of 2 positive integers")
+        finally:
+            self.__position = value
