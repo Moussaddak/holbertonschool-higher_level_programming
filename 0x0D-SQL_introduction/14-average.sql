54c4198ee6f3a14cd973f01cf3b5b184feb09ec1
moussaddak <meddeb.moussaddak@gmail.com>
2020-07-16 11:49:17 +0000

    SQL commit

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- 14. Average
+-- computes the score average of all records in the table second_table
+SELECT AVG(score)AS average FROM second_table;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- 16. Say my name
+-- Lists all records of the table second_table of the database hbtn_0c_0
+SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
