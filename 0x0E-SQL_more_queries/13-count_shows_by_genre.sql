95a30412c7cc15927564f4023e81084ba874954d
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-07-17 22:56:56 +0200

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;
