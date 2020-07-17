4795952b70ac56918e2c2fd991cef84c68d3ecdb
moussaddak <meddeb.moussaddak@gmail.com>
2020-07-17 21:56:55 +0000

    more SQL commit

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- 15. Only Comedy
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
