a130ca108e44cb8c38772a34a13db5e0c29135d8
moussaddak <meddeb.moussaddak@gmail.com>
2020-08-11 22:02:02 +0000

    update solution

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+  Takes in an argument and displays all values in the states table
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                     port=3306,
+                     user=argv[1],
+                         passwd=argv[2],
+                     db=argv[3],
+                     charset="utf8")
+
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM states WHERE name=('{}') ORDER BY id".format(argv[4]))
+        rows = cursor.fetchall()
+        for row in rows:
+            if row[1] == argv[4]:
+                print(row)
