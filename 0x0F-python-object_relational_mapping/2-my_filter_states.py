d79f6e8bcd915e2f6102db825dcd1cae81ec8b1e
moussaddak <meddeb.moussaddak@gmail.com>
2020-08-11 22:05:13 +0000

    update pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,15 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
+                         port=3306,
+                         user=argv[1],
                          passwd=argv[2],
-                     db=argv[3],
-                     charset="utf8")
+                         db=argv[3],
+                         charset="utf8")
 
     with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states WHERE name=('{}') ORDER BY id".format(argv[4]))
+        cursor.execute("SELECT * FROM states WHERE name=('{}') ORDER BY id"
+                       .format(argv[4]))
         rows = cursor.fetchall()
         for row in rows:
             if row[1] == argv[4]:
