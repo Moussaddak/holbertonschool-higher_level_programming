2fad9c7a4f83a6e09abe0d4517ab9ef3df395a98
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-12-14 19:05:31 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,12 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
 
     with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY ('{}')\
+#first solution
+      cursor.execute("SELECT * FROM states WHERE name LIKE BINARY ('{}')\
                           ORDER BY id".format(argv[4]))
+#second solution:
+#        cursor.execute("SELECT * FROM states WHERE name= %(name)s ORDER BY id",
+#                       {'name': argv[4]})
         rows = cursor.fetchall()
         for row in rows:
             print(row)
