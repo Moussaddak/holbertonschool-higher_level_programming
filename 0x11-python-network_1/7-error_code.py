448c51c0fcc7af8568dbbc4e1be96b55ccb26186
moussaddak <meddeb.moussaddak@gmail.com>
2021-01-11 22:25:17 +0000

    update permission

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Error code #1 """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    print(r.text)
