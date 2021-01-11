49a4c0875c9becdb51ab57254e49a38c42a4b097
moussaddak <meddeb.moussaddak@gmail.com>
2021-01-11 19:04:39 +0000

    pep8 update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@ from urllib.request import urlopen
 
 url = 'https://intranet.hbtn.io/status'
 with urlopen(url) as r:
-  data = r.read()
-  print("Body response:")
-  print("\t- type: {}".format(type(data)))
-  print("\t- content: {}".format(data))
-  print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    data = r.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode("utf-8")))
